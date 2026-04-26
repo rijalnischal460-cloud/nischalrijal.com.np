c85a5a0ce571b65e9b09546b1140d0522df6cef1
Nischal Rijal <245581786+rijalnischal460-cloud@users.noreply.github.com>
2026-04-27 01:44:09 +0545

    Delete nischal-rijal---portfolio directory

--- nischal-rijal---portfolio/check_fetch.js
+++
@@ -1,6 +0,0 @@
-
-const desc = Object.getOwnPropertyDescriptor(window, 'fetch');
-console.log('Fetch descriptor:', desc);
-if (desc && desc.set === undefined && desc.writable === false) {
-  console.log('Fetch is read-only');
-}
